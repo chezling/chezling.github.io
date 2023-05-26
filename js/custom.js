7dcd372b2d3bcf3443cbfaaa5e2c9abb3c573f8c
Ling Fei MA <82127235+chezling@users.noreply.github.com>
2023-05-26 15:41:41 -0400

    10th

--- js/custom.js
+++ js/custom.js
@@ -13,10 +13,28 @@ $(function() {
 
     /* Hide mobile menu after clicking on a link
     -----------------------------------------------*/
-    $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
+    // $('.navbar-collapse a').click(function(){
+    //     $(".navbar-collapse").collapse('hide');
+    // });
    
 })
 
 
+$(document).ready(function() {
+    $('.submenu').click(function(e) {
+      e.preventDefault();
+      var selectedTag = $(this).data('tag');
+  
+      if (selectedTag === 'All') {
+        $('.item').show();
+      } else {
+        $('.item').hide();
+        $('.item[data-tags*="' + selectedTag + '"]').show();
+      }
+  
+      // Collapse the navigation menu after clicking on a submenu item on mobile
+      if ($(window).width() < 768) {
+        $('.navbar-collapse').collapse('hide');
+      }
+    });
+  });
